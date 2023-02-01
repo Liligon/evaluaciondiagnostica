1033f2d67a28e0aa46fce99eefcb1252f4d7d8ab
Liliana <liliana87gonzalez@gmail.com>
2023-02-01 13:10:16 -0300

    evaluacion diagnostica

---
+++ main.js
@@ -0,0 +1,25 @@
+const catalogo= [ 
+{
+    id= 1
+    producto= "reloj"
+    precio=  300
+}
+
+{
+    id= 2
+    producto= "audifonos"
+    precio= 200
+}
+{
+    id= 3
+    producto= "audifonos"
+    precio= 100
+}
+{
+    id= 4
+    producto= "silla"
+    precio= 400
+}
+
+]
+
